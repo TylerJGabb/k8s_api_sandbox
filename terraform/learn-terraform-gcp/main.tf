e3d50c33a1de70dd30a45d3e604015ec55b2d701
Tyler Gabb <tylerjgabb@gmail.com>
2023-05-10 22:28:27 -0700

    introduce destructive change

--- terraform/learn-terraform-gcp/main.tf
+++ terraform/learn-terraform-gcp/main.tf
@@ -25,7 +25,7 @@ resource "google_compute_instance" "vm_instance" {
   tags         = ["web", "dev"]
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = "cos-cloud/cos-stable"
     }
   }
   network_interface {
