2640c44399b44b70e6bd7ab10ef295c4109dbc5b
Tyler Gabb <tylerjgabb@gmail.com>
2023-05-10 22:22:54 -0700

    add a vm instance

--- terraform/learn-terraform-gcp/main.tf
+++ terraform/learn-terraform-gcp/main.tf
@@ -19,4 +19,21 @@ resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
-
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+      // Include this section to give the VM an external ip address
+      // TODO: create a google_compute_firewall resource to allow external traffic to this VM
+      // https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_firewall
+    }
+  }
+}
