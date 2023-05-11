82cce9f872e2e978cba014a291a988015008e08f
Tyler Gabb <tylerjgabb@gmail.com>
2023-05-11 09:36:05 -0700

    creating pod agent with permissions

--- terraform/actual_terraform/iam.tf
+++ terraform/actual_terraform/iam.tf
@@ -1,9 +1,19 @@
-# resource "google_service_account" "main_sa" {
-#   account_id = "gke-pod-agent"
-# }
+resource "google_service_account" "pod_agent" {
+  account_id = "gke-pod-agent"
+}
 
-# resource "google_project_iam_member" "pod_agent_editor" {
-#   project = var.project
-#   role    = "roles/Editor"
-#   member  = "serviceAccount:${google_service_account.main_sa.email}"
-# }
+resource "google_project_iam_member" "pod_agent_wi_user" {
+  project = var.project
+  role    = "roles/iam.workloadIdentityUser"
+  member  = "serviceAccount:${google_service_account.pod_agent.email}"
+}
+
+resource "google_project_iam_member" "pod_agent_sa_token_creator" {
+  project = var.project
+  role    = "roles/iam.serviceAccountTokenCreator"
+  member  = "serviceAccount:${google_service_account.pod_agent.email}"
+}
+
+output "pod_agent_sa_email" {
+  value = google_service_account.pod_agent.email
+}
