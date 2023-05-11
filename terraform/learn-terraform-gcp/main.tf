af0ad617b7d631a970715014d6bf0f764722012f
Tyler Gabb <tylerjgabb@gmail.com>
2023-05-10 22:13:19 -0700

    initialized a few tf sandboxes

---
+++ terraform/learn-terraform-gcp/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("../../tf-agent-credentials.json")
+
+  project = "a-proj-to-be-deleted"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+
