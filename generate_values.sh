3a4b2b0874cd5369a0a73cb615e28b0ad2242d6a
Tyler Gabb <tylerjgabb@gmail.com>
2023-05-18 19:56:52 -0700

    prepping to port

--- generate_values.sh
+++ generate_values.sh
@@ -1,4 +1,4 @@
 ROOT=$(PWD)
 cd terraform
 terraform output -json | jq -r 'to_entries[] | "\(.key): \(.value.value)"' >>$ROOT/helm-charts/main/values.yaml
-terraform output -json | jq -r 'to_entries[] | "\(.key): \(.value.value)"' >$ROOT/helm-charts/configconnector/values.yaml
+terraform output -json | jq -r 'to_entries[] | "\(.key): \(.value.value)"' >>$ROOT/helm-charts/configconnector/values.yaml
